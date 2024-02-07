33b2ed301485bff4e973288eb0a4288a8127e66c
Hassancoding21 <hhaassaann2100@gmail.com>
2024-02-07 13:32:23 +0500

    all files added

---
+++ app.js
@@ -0,0 +1,15 @@
+// alert("this is my 6 class");
+
+var myname;
+myname = "hassan";
+var myname ="hassan"
+
+var firstname = "mirza ";
+var lastname = "hassan";
+var fullname = firstname + lastname;
+console.log(fullname)
+
+var num1 = 7;
+var num2 = 5
+var total = num1 + num2;
+console.log(total)
